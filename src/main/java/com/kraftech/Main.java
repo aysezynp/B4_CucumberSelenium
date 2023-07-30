56a307057c8805b5b69387fe1dfff4d306fff75e
aysezynp <128591659+aysezynp@users.noreply.github.com>
2023-07-30 21:22:19 +0300

    commit last

--- src/main/java/com/kraftech/Main.java
+++ src/main/java/com/kraftech/Main.java
@@ -3,5 +3,8 @@ package com.kraftech;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(System.getenv("password"));//Sistemdeki ortam değişkenlerindeki şifreyi alma
+                                                            //Security precaution
+
     }
 }
